bb583a26430a4d988239f1bd142d642084fc5aa1
ryuma017 <tgcrym01@gmail.com>
2022-02-03 01:56:41 +0900

    add

---
+++ fahrenheit_to_celsius_conversion/src/main.rs
@@ -0,0 +1,16 @@
+fn convert_fahrenheit_into_celsius(f: i32) -> i32 {
+    (f - 32) * 5 / 9
+}
+
+fn main() {
+    let mut i = String::new();
+    std::io::stdin()
+        .read_line(&mut i)
+        .ok();
+
+    let i: i32 = i.trim().parse().ok().unwrap();
+
+    let converted_i = convert_fahrenheit_into_celsius(i);
+
+    println!("{}", converted_i);
+}
