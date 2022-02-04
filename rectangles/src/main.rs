b9b5fdb45ff150f2491c8778ffcd738273dfef10
ryuma017 <tgcrym01@gmail.com>
2022-02-04 19:01:59 +0900

    fmt

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -15,9 +15,18 @@ impl Rectangle {
 }
 
 fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
-    let rect2 = Rectangle { width: 10, height: 40 };
-    let rect3 = Rectangle { width: 60, height: 45 };
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels",
