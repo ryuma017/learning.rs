98d6595693e14ab8b8a4260dc3a85a4b0e3c9463
ryuma017 <tgcrym01@gmail.com>
2022-03-10 16:29:18 +0900

    define `hashmap!`

---
+++ macro/src/bin/macro_hashmap.rs
@@ -0,0 +1,25 @@
+macro_rules! hashmap {
+    ( $($key:expr => $val:expr),* ) => {{
+        // HashMapを生成
+        let mut tmp = std::collections::HashMap::new();
+        $(
+            tmp.insert($key, $val);
+        )*
+        tmp
+    }}
+}
+
+fn main() {
+    // マクロを利用してHashMapを初期化 --- (*5)
+    let week = hashmap![
+        "mon" => "月曜",
+        "tue" => "火曜",
+        "wed" => "水曜",
+        "thu" => "木曜",
+        "fri" => "金曜",
+        "sat" => "土曜",
+        "sun" => "日曜"
+    ];
+    println!("mon={}", week["mon"]);
+    println!("wed={}", week["wed"]);
+}
