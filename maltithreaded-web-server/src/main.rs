08d4a64f915d9801a3b75d6350cafc652d8b78ab
ryuma017 <tgcrym01@gmail.com>
2022-07-02 11:57:49 +0900

    reading from the `TcpStream` and printing the data

--- maltithreaded-web-server/src/main.rs
+++ maltithreaded-web-server/src/main.rs
@@ -1,4 +1,5 @@
-use std::net::TcpListener;
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -6,6 +7,15 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+#[allow(clippy::unused_io_amount)]
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
