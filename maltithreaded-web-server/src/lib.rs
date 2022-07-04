9bdc0989d85444426518b03821f141216e5ef8de
ryuma017 <tgcrym01@gmail.com>
2022-07-04 17:12:07 +0900

    modifying `ThreadPool` to hold `Worker` instances instead of holding threads directly

--- maltithreaded-web-server/src/lib.rs
+++ maltithreaded-web-server/src/lib.rs
@@ -1,7 +1,7 @@
 use std::thread;
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
@@ -15,13 +15,13 @@ impl ThreadPool {
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {
-            // create some threads and store them in the vector
+        for id in 0..size {
+            workers.push(Worker::new(id));
         }
 
-        Self { threads }
+        Self { workers }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -29,5 +29,18 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static
     {
         unimplemented!()
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker { id, thread }
     }
 }
