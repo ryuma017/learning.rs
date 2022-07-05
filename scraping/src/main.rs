2af97a1bae1948b64105159842ee9fe6658831a1
ryuma017 <tgcrym01@gmail.com>
2022-07-05 23:45:53 +0900

    get html content

--- scraping/src/main.rs
+++ scraping/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Url;
+
+const QIITA_URL: &str = "https://qiita.com";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let target_url = Url::parse(QIITA_URL)?.join("tags/rust")?;
+    let client = reqwest::Client::new();
+    let html = client.get(target_url).send().await?.text().await?;
+
+    println!("{}", html);
+    Ok(())
 }
