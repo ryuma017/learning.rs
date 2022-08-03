7659ea93fb97609f86073d355ab064ebf20fcdc9
ryuma017 <tgcrym01@gmail.com>
2022-08-03 23:35:26 +0900

    binary search tree

--- binary_search_tree/src/lib.rs
+++ binary_search_tree/src/lib.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 #[derive(Debug)]
 pub struct Node {
     data: isize,
@@ -7,18 +9,84 @@ pub struct Node {
 
 impl Node {
     pub fn new(num: isize) -> Self {
-        Self { data: num, left: None, right: None }
+        Self {
+            data: num,
+            left: None,
+            right: None,
+        }
     }
 
     pub fn insert(&mut self, num: isize) {
-        todo!()
+        match &self.data.cmp(&num) {
+            Ordering::Greater => {
+                if let Some(left_node) = &mut self.left {
+                    left_node.insert(num);
+                } else {
+                    let node = Self::new(num);
+                    self.left = Some(Box::new(node));
+                }
+            }
+            Ordering::Less => {
+                if let Some(right_node) = &mut self.right {
+                    right_node.insert(num);
+                } else {
+                    let node = Self::new(num);
+                    self.right = Some(Box::new(node));
+                }
+            }
+            Ordering::Equal => {}
+        }
     }
 
     pub fn search(&self, num: isize) -> bool {
-        todo!()
+        match &self.data.cmp(&num) {
+            Ordering::Greater => {
+                if let Some(left_node) = &self.left {
+                    left_node.search(num)
+                } else {
+                    false
+                }
+            }
+            Ordering::Less => {
+                if let Some(right_node) = &self.right {
+                    right_node.search(num)
+                } else {
+                    false
+                }
+            }
+            Ordering::Equal => true,
+        }
     }
 
     pub fn delete(&mut self, num: isize) -> Option<isize> {
-        todo!()
+        match &self.data.cmp(&num) {
+            Ordering::Greater => {
+                if let Some(left_node) = &mut self.left {
+                    if left_node.data.cmp(&num).is_eq() {
+                        self.left = None;
+                        return Some(num);
+                    }
+                    left_node.delete(num)
+                } else {
+                    None
+                }
+            }
+            Ordering::Less => {
+                if let Some(right_node) = &mut self.right {
+                    if right_node.data.cmp(&num).is_eq() {
+                        self.right = None;
+                        return Some(num);
+                    }
+                    right_node.delete(num)
+                } else {
+                    None
+                }
+            }
+            Ordering::Equal => {
+                self.left = None;
+                self.right = None;
+                Some(num)
+            }
+        }
     }
 }
