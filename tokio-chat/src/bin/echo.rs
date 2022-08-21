9ddb26c4ee64976556a3dfc53250953d951e0455
ryuma017 <tgcrym01@gmail.com>
2022-08-21 23:51:52 +0900

    tokio-chat: echo server

--- tokio-chat/src/bin/echo.rs
+++ tokio-chat/src/bin/echo.rs
@@ -1,4 +1,27 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
+    net::TcpListener,
+};
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    loop {
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        tokio::spawn(async move {
+            let (reader, mut writer) = socket.split();
+
+            let mut reader = BufReader::new(reader);
+
+            loop {
+                let mut msg = String::new();
+                let bytes_len = reader.read_line(&mut msg).await.unwrap();
+                if bytes_len == 0 {
+                    break;
+                }
+                writer.write_all(msg.as_bytes()).await.unwrap();
+            }
+        });
+    }
 }
